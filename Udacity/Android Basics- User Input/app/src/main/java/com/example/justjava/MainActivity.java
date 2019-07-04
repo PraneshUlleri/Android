c0b17a844671f9b1b1b5a40df77f22d7f6c5d202
Pranesh <ipraneshulleri@gmail.com>
2019-07-04 05:43:11 +0530

    adding the coffee cup app

--- Udacity/Android Basics- User Input/app/src/main/java/com/example/justjava/MainActivity.java
+++ Udacity/Android Basics- User Input/app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.justjava;
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
-
+ int quant=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+     private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(quant);
+        displayPrice(quant*5);
+    }
+    public void inc(View view ){
+         quant++;
+         display(quant);
+    }
+    public void dec(View view){
+        quant--;
+        display(quant);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.textView2);
+        quantityTextView.setText("" + number);
     }
 }
