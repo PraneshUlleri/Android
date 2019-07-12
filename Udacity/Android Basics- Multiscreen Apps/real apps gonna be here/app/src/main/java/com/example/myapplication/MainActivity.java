4e44686c4a8cc94dcbdb181eedf66566a46bc4bf
Pranesh <ipraneshulleri@gmail.com>
2019-07-13 05:15:55 +0530

    adding a new project

--- Udacity/Android Basics- Multiscreen Apps/real apps gonna be here/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Udacity/Android Basics- Multiscreen Apps/real apps gonna be here/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void family(View view){
+        Intent intent=new Intent(this,FamilyActivity.class);
+        startActivity(intent);
+    }
+    public void color(View view){
+        Intent intent = new Intent (this, ColorsActivity.class);
+        startActivity(intent);
+    }
+
+    public void phase(View view){
+        Intent intent = new Intent (this, PhasesActivity.class);
+        startActivity(intent);
+    }
+
+    public void number(View view){
+        Intent intent = new Intent (this, NumbersActivity.class);
+        startActivity(intent);  
+    }
+
+
 }
