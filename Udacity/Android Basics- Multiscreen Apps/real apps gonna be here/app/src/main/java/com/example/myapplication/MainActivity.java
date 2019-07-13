78a7fa43592438333b36e34f813c4655b936febf
Pranesh <ipraneshulleri@gmail.com>
2019-07-14 02:42:33 +0530

    adding arraylist

--- Udacity/Android Basics- Multiscreen Apps/real apps gonna be here/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Udacity/Android Basics- Multiscreen Apps/real apps gonna be here/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//
+//        Button buttonfam= (Button) findViewById(R.id.fam);
+//        buttonfam.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//                    public void onClick(View v){}
+//        }
+//                );
     }
 
     public void family(View view){
@@ -29,8 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void number(View view){
         Intent intent = new Intent (this, NumbersActivity.class);
-        startActivity(intent);  
+        startActivity(intent);
     }
-
 
 }
--- Udacity/Android Basics- Multiscreen Apps/real apps gonna be here/app/src/main/java/com/example/myapplication/NumbersActivity.java
+++ Udacity/Android Basics- Multiscreen Apps/real apps gonna be here/app/src/main/java/com/example/myapplication/NumbersActivity.java
@@ -2,6 +2,10 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+        ArrayList<String>word= new ArrayList<String>();
+
+        word.add("zero");
+        word.add("one");
+        word.add("two");
+        word.add("three");
+        word.add("four");
+        word.add("five");
+        word.add("six");
+        word.add("seven");
+        word.add("eight");
+        word.add("nine");
+        word.add("ten");
+        LinearLayout root = (LinearLayout)findViewById(R.id.numbers);
+        TextView wordView= new TextView(this);
+        for (int i=0;i<word.size();i++){
+            wordView.setText(word.get(i));
+
+        }
+        root.addView(wordView);
+
     }
 }
